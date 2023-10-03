4cc0b0028b18ff40812737c0e2f0d3e525b48040
Đặng Hoàn Phương <146814457+DangHoanPhuong@users.noreply.github.com>
2023-10-03 22:01:05 +0700

    Update demo.kt
    Phương cập nhật thêm hàm Xuất thí sinh trúng tuyển và sắp xếp danh sách

--- demo.kt
+++ demo.kt
@@ -24,6 +24,34 @@ class ThiSinh(cccd: String, hoTen: String, toan: Float, ly: Float, hoa: Float, v
     //Ham khoi tao khong tham so
     constructor() : this("", "", 0f, 0f, 0f, 0f, 0f, "")
 }
+
+fun SapXepDS(ds:ArrayList<sinhvien>){
+    var temp:sinhvien;
+    for(i in  0..ds.size-1){
+        for(j in i+1..ds.size-1){
+            if(ds[i].SoSanhTen(ds[j])>0){
+                temp=ds[i];
+                ds[i]=ds[j];
+                ds[j]=temp;
+            }
+        }
+    }
+}
+
+fun SinhVienTrungTuyen(DS: ArrayList<sinhvien>,diemchuan:Float){
+    var boolean=false;
+    println("---Danh sách thí sinh trúng tuyển---")
+    for (i in 0..DS.size-1){
+        if(DS[i].TongDiem()>=diemchuan){
+            println(DS[i])
+            boolean=true;
+        }
+    }
+    if(!boolean){
+        println("--Không có thí sinh trúng tuyển!--")
+    }
+}
+
 fun main() {
    
 }
